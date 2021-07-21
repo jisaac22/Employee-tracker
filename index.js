c23c396b67109624697b157279bd3bc3e04588af
Jacob Sierra <jacobisaac22@yahoo.com>
2021-07-21 09:55:13 -0500

    added connection for mysql databasw

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // packages to run app
 const inquirer = require('inquirer')
 const fs = require('fs')
+const mysql = require('mysql')
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'jisaac22',
+  database: 'employeeTracker_db'
+});
 
 // First question for Employers choice
 const employeeUpdate = () => {
